8ce8e5dc52a9fc59fee20d6ea2507bc8ec3f4f9f
juliem35 <juliettemugisa35@gmail.com>
2025-06-25 09:42:21 +0100

    Version finale depuis video_A

--- server.js
+++ server.js
@@ -13,30 +13,42 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-    console.log("Nouvel utilisateur connecté");
+    console.log("Un utilisateur s'est connecté :", socket.id);
 
-    socket.on("join", (roomId) => {
+    socket.on("demarrer-appel", (roomId) => {
         socket.join(roomId);
-        socket.to(roomId).emit("user-connected", socket.id);
+        socket.to(roomId).emit("appel-recu", socket.id);
+    });
 
-        socket.on("offer", (data) => {
-            socket.to(roomId).emit("offer", data);
-        });
+    socket.on("rejoindre-appel", (roomId) => {
+        socket.join(roomId);
+    });
 
-        socket.on("answer", (data) => {
-            socket.to(roomId).emit("answer", data);
-        });
+    socket.on("accepter-appel", ({ roomId, from }) => {
+        io.to(from).emit("appel-accepte", socket.id);
+    });
 
-        socket.on("candidate", (data) => {
-            socket.to(roomId).emit("candidate", data);
-        });
+    socket.on("refuser-appel", ({ roomId, from }) => {
+        io.to(from).emit("appel-refuse");
+    });
 
-        socket.on("disconnect", () => {
-            socket.to(roomId).emit("user-disconnected", socket.id);
-        });
+    socket.on("offer", ({ to, offer }) => {
+        io.to(to).emit("offer", { offer, from: socket.id });
+    });
+
+    socket.on("answer", ({ to, answer }) => {
+        io.to(to).emit("answer", { answer, from: socket.id });
+    });
+
+    socket.on("candidate", ({ to, candidate }) => {
+        io.to(to).emit("candidate", { candidate, from: socket.id });
     });
 });
 
 server.listen(PORT, () => {
+<<<<<<< HEAD
     console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+=======
+    console.log("Serveur en ligne sur le port", PORT);
+>>>>>>> 116f9d2 (Version finale depuis video_A)
 });
